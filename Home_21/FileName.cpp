45433673f7dcab5c5f18d542b2afdb279b5e6d60
Слава <@KOMPUTER>
2023-12-10 17:56:08 +0500

    Добавьте файлы проекта.

---
+++ Home_21/FileName.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	// Задание 1
+	int X, Y;
+	std::cout << "Введите два числа -> ";
+	std::cin >> X >> Y;
+	int* px = &X;
+	int* py = &Y;
+	int tmp = *px;
+	*px = *py;
+	*py = tmp;
+	std::cout << " X = " << X << "\n";
+	std::cout << " Y = " << Y << "\n";
+
+	return 0;
+}
